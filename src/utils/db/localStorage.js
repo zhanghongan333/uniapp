97cc658b99f26245bd1b779e944737265b2f5ec1
张洪安 <1838344185@qq.com>
2023-03-27 23:31:39 +0800

    优化

--- src/utils/db/localStorage.js
+++ src/utils/db/localStorage.js
@@ -3,7 +3,6 @@ class LocalStorage extends Base {
   read() {
     const data = uni.getStorageSync(this.source)
     if (data && data !== '') {
-      console.log(data)
       return this.deserialize(data)
     } else {
       uni.setStorageSync(this.source, this.serialize(this.defaultValue))
